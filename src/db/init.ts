26de5db53dbd1505aaadaf5090017fb6e6013e4c
Jarrku <simonvdbroeck@gmail.com>
2017-09-25 19:52:19 +0200

    Plug in native promises

--- src/db/init.ts
+++ src/db/init.ts
@@ -1,18 +1,21 @@
-import { connect, connection } from "mongoose";
+import mongoose = require("mongoose");
 
 const initDb = () => {
   const { DB_USERNAME, DB_PASSWORD } = process.env;
 
   const connectionString = `mongodb://${DB_USERNAME}:${DB_PASSWORD}@ds151014.mlab.com:51014/lom`;
 
-  connect(connectionString,
+  mongoose.connect(connectionString,
     {
       useMongoClient: true,
       promiseLibrary: global.Promise,
     });
 
-  connection.on("error", console.error.bind(console, "connection error:"));
-  connection.once("open", () => console.log("Connected to DB"));
+  mongoose.connection.on("error", console.error.bind(console, "connection error:"));
+  mongoose.connection.once("open", () => {
+    mongoose.Promise = global.Promise;
+    console.log("Connected to DB");
+  });
 };
 
 export default initDb;
