fa930af42adced004f5b1b95754a9fe094ba4159
Jarrku <simonvdbroeck@gmail.com>
2017-09-27 16:09:39 +0200

    timeframeArgs to common

--- src/common.ts
+++ src/common.ts
@@ -13,3 +13,46 @@ export const isInRoleCollection = (roles: Collection<string, Role>, ...rolesToFi
 
   return index > -1;
 };
+
+// Timeframe shared code
+
+const validStrings = [...config.weekStrings, ...config.dayStrings];
+const timeframeParser = (val: string) => {
+  const currDate = new Date().valueOf();
+
+  const letr = val.match(/[a-zA-Z]+/g);
+  const num = val.match(/\d+/g);
+
+  if (letr && num) {
+    const [weekOrDay] = letr;
+    const [amountStr] = num;
+    let amount = Number.parseInt(amountStr);
+
+    if (config.weekStrings.includes(weekOrDay))
+      amount *= 7;
+
+    return currDate - (amount * 3600000 * 24);
+  }
+
+  return 0;
+};
+
+const timeframeValidator = (val: string) => {
+  const letr = val.match(/[a-zA-Z]+/g);
+  const num = val.match(/\d+/g);
+
+  if (letr && num) {
+    return validStrings.includes(letr[0]);
+  }
+  return false;
+};
+
+export const timeframeArg = {
+  key: "timeframe",
+  label: "How long ago",
+  prompt: "Shouldnt show",
+  default: 0,
+  type: "string",
+  parse: timeframeParser,
+  validate: timeframeValidator,
+};
