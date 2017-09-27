2bb29849b52461fe741576b94d7ce1d55a2a0a30
Jarrku <simonvdbroeck@gmail.com>
2017-09-27 10:39:34 +0200

    Constructor auto init params

--- src/db/user.ts
+++ src/db/user.ts
@@ -2,26 +2,22 @@ import { Message as DMessage, TextChannel } from "discord.js";
 import { IMessage, IUser, IUserModel, User as UserDB } from "../model/user";
 
 export class User {
-  private _userModel: IUserModel;
-
-  constructor(userModel: IUserModel) {
-    this._userModel = userModel;
-  }
+  constructor(readonly userModel: IUserModel) { }
 
   get messages() {
-    return this._userModel.messages;
+    return this.userModel.messages;
   }
 
   get discordId() {
-    return this._userModel.discordId;
+    return this.userModel.discordId;
   }
 
   addMessage(msg: IMessage) {
-    this._userModel.messages.push(msg);
+    this.userModel.messages.push(msg);
     this.save();
   }
 
   save() {
-    this._userModel.save((err: any) => { if (err) console.log("Error in save: ", err); });
+    this.userModel.save((err: any) => { if (err) console.log("Error in save: ", err); });
   }
 }
