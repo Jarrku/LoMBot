51ca7ab0a928427e9d2d6aa0cfdcd817c2123248
Jarrku <simonvdbroeck@gmail.com>
2017-09-27 20:47:35 +0200

    Add more env vars

--- src/db/init.ts
+++ src/db/init.ts
@@ -1,13 +1,14 @@
 import mongoose = require("mongoose");
 
 export default () => {
-  const { DB_USERNAME, DB_PASSWORD } = process.env;
-
-  const connectionString = `mongodb://${DB_USERNAME}:${DB_PASSWORD}@ds151014.mlab.com:51014/lom`;
+  const { DB_USERNAME, DB_PASSWORD, DB_HOSTNAME, DB_NAME } = process.env;
+  const connectionString = `mongodb://${DB_USERNAME}:${DB_PASSWORD}@${DB_HOSTNAME}/${DB_NAME}`;
 
   mongoose.connect(connectionString, {
     useMongoClient: true,
     promiseLibrary: global.Promise,
+    keepAlive: 120,
+    ssl: true,
   });
 
   mongoose.connection.on("error", console.error.bind(console, "connection error:"));
